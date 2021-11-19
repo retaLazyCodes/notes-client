190d4429898656946ea6b3642a0cb40d24179f8f
Brian Retamar <leo1632@hotmail.com>
2021-11-19 11:04:22 -0300

    Added action-creators & test

--- src/redux/reducers/noteReducer.js
+++ src/redux/reducers/noteReducer.js
@@ -2,11 +2,12 @@
 export function noteReducer(state = [], action) {
     switch (action.type) {
         case '@notes/created':
-            return state.concat(action.payload)
-        case '@notes/toggle_importance':
+            return [...state, action.payload]
+
+        case '@notes/toggle_important':
             const { id } = action.payload
             return state.map(note => {
-                if (note.id == id) {
+                if (note.id === id) {
                     return {
                         ...note,
                         important: !note.important
@@ -16,5 +17,27 @@ export function noteReducer(state = [], action) {
             })
         default:
             return state
+    }
+}
+
+const generateId = () => Math.floor(Math.random() * 99999) + 1
+
+export const createNote = (content) => {
+    return {
+        type: '@notes/created',
+        payload: {
+            content,
+            important: false,
+            id: generateId()
+        }
+    }
+}
+
+export const toggleImportanceOf = (id) => {
+    return {
+        type: '@notes/toggle_important',
+        payload: {
+            id
+        }
     }
 }
