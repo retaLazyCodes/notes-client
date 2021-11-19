0cdeb851134d9e69d6ce5a3e5b50e7e171c363fc
Brian Retamar <leo1632@hotmail.com>
2021-11-19 11:05:02 -0300

    refactor, cleaned App component

--- src/App.js
+++ src/App.js
@@ -1,48 +1,16 @@
-import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import NoteForm from './components/NoteForm';
+import Notes from './components/Notes';
+
 
 function App() {
-  const notes = useSelector(state => state)
-  const dispatch = useDispatch()
-
-  const handleAddNote = (e) => {
-    e.preventDefault()
-    const { target } = e
-    const content = target.note.value
-    target.note.value = ''
-    dispatch({
-      type: '@notes/created',
-      payload: {
-        content,
-        important: false,
-        id: 1
-      }
-    })
-  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <form onSubmit={handleAddNote}>
-          <input name="note" />
-          <button>add</button>
-        </form>
-        <ul>
-          {
-            notes.map(note => {
-              return <li key={note.id}>
-                {note.content}
-                <strong>
-                  {
-                    note.important
-                      ? ' important'
-                      : ' not important'
-                  }
-                </strong>
-              </li>
-            })
-          }
-        </ul>
+        <h1>App de notas</h1>
+        <NoteForm />
+        <Notes />
       </header>
     </div>
   );
